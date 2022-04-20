cf10a77cd71b95c3fc07bd18b94aea42e4480c9a
triviadraft <triviadraft@gmail.com>
2022-04-19 18:49:03 -0700

    rename random_item to randomItem

--- collect_tagline_data.js
+++ collect_tagline_data.js
@@ -2,9 +2,9 @@ function randomItem(items) {
   return items[Math.floor(Math.random()*items.length)];   
 }
 
-var movie = random_item(Object.keys(tagline_map));
-var category = random_item(movie_map[movie]['categories'].split(","))
-var random_tagline = random_item(tagline_map[movie])
+var movie = randomItem(Object.keys(tagline_map));
+var category = randomItem(movie_map[movie]['categories'].split(","))
+var random_tagline = randomItem(tagline_map[movie])
 
 var question = 'What ' + category + ' film has the tagline "' + random_tagline + '"?'
 var answer = movie + '(' + movie_map[movie]['year'] + ')'
