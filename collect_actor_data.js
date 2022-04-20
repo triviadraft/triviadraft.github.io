0ee247313a73750a1f7104434951a2146b2f4173
triviadraft <triviadraft@gmail.com>
2022-04-20 02:03:25 -0700

    remove utils

--- collect_actor_data.js
+++ collect_actor_data.js
@@ -1,5 +1,5 @@
 function randomItem(items) {
-  return items[Math.floor(Math.random()*items.length)];   
+  return items[Math.floor(Math.random()*items.length)];
 }
 
 function randomItems(n, items) {
@@ -7,25 +7,67 @@ function randomItems(n, items) {
     return shuffled.slice(0, n);
 }
 
+function displayActorsAnswer() { 
+    document.getElementById('answer').innerHTML = answer;
+    document.getElementById('answer1').innerHTML = answer1;
+    document.getElementById('answer2').innerHTML = answer2;
+    document.getElementById('answer3').innerHTML = answer3;
+}
+
+function resetActors() { 
+    var movie = randomItem(Object.keys(actor_map));
+    var category = randomItem(movie_map[movie]['categories'].split(","));
+    var actor_list = actor_map[movie]['supporting'];
+    var random_list = randomItems(3, actor_list);
+
+    var actor1 = random_list[0]['actor'];
+    var actor2 = random_list[1]['actor'];
+    var actor3 = random_list[2]['actor'];
+    var name1 = random_list[0]['name'];
+    if (random_list[0]['description']) {
+        name1 += ' (' + random_list[0]['description'] + ')';
+    }
+    var name2 = random_list[1]['name'];
+    if (random_list[1]['description']) {
+        name2 += ' (' + random_list[1]['description'] + ')';
+    }
+    var name3 = random_list[2]['name'];
+    if (random_list[2]['description']) {
+        name3 += ' (' + random_list[2]['description'] + ')';
+    }
+
+    question = 'What ' + category + ' film includes ' + actor1 +', ' + actor2 + ', and ' + actor3 + '?';
+    answer = movie + ':';
+    answer1 = actor1 + ' as ' + name1;
+    answer2 = actor2 + ' as ' + name2;
+    answer3 = actor3 + ' as ' + name3;
+    
+    document.getElementById('question').innerHTML = question;
+    document.getElementById('answer').innerHTML = '';
+    document.getElementById('answer1').innerHTML = '';
+    document.getElementById('answer2').innerHTML = '';
+    document.getElementById('answer3').innerHTML = '';
+}
+
 var movie = randomItem(Object.keys(actor_map));
 var category = randomItem(movie_map[movie]['categories'].split(","));
-var actor_list = actor_map[movie]['supporting']
-var random_list = randomItems(3, actor_list)
+var actor_list = actor_map[movie]['supporting'];
+var random_list = randomItems(3, actor_list);
 
-var actor1 = random_list[0]['actor']
-var actor2 = random_list[1]['actor']
-var actor3 = random_list[2]['actor']
-var name1 = random_list[0]['name']
+var actor1 = random_list[0]['actor'];
+var actor2 = random_list[1]['actor'];
+var actor3 = random_list[2]['actor'];
+var name1 = random_list[0]['name'];
 if (random_list[0]['description']) {
-    name1 += ' (' + random_list[0]['description'] + ')'
+    name1 += ' (' + random_list[0]['description'] + ')';
 }
-var name2 = random_list[1]['name']
+var name2 = random_list[1]['name'];
 if (random_list[1]['description']) {
-    name2 += ' (' + random_list[1]['description'] + ')'
+    name2 += ' (' + random_list[1]['description'] + ')';
 }
-var name3 = random_list[2]['name']
+var name3 = random_list[2]['name'];
 if (random_list[2]['description']) {
-    name3 += ' (' + random_list[2]['description'] + ')'
+    name3 += ' (' + random_list[2]['description'] + ')';
 }
 
 var question = 'What ' + category + ' film includes ' + actor1 +', ' + actor2 + ', and ' + actor3 + '?';
--- collect_tagline_data.js
+++ collect_tagline_data.js
@@ -1,40 +1,39 @@
 function randomItem(items) {
-  return items[Math.floor(Math.random()*items.length)];  
+  return items[Math.floor(Math.random()*items.length)];
 }
 
 function randomItems(n, items) {
-    var shuffled = items.sort(() => 0.5 - Math.random())
-    return shuffled.slice(0, n)
+    var shuffled = items.sort(() => 0.5 - Math.random());
+    return shuffled.slice(0, n);
 }
 
-var movies = randomItems(5, Object.keys(tagline_map))
+var movies = randomItems(5, Object.keys(tagline_map));
 
-questions = []
-answers = []
+questions = [];
+answers = [];
 for (const movie of movies) {
-    var category = randomItem(movie_map[movie]['categories'].split(","))
-    var random_tagline = randomItem(tagline_map[movie])
-    var question = 'What ' + category + ' film has the tagline "' + random_tagline + '"?'
-    var answer = movie
+    var category = randomItem(movie_map[movie]['categories'].split(","));
+    var random_tagline = randomItem(tagline_map[movie]);
+    var question = 'What ' + category + ' film has the tagline "' + random_tagline + '"?';
+    var answer = movie;
 
     if (!movie.includes('(1') && !movie.includes('(2')) {
-        var year = movie_map[movie]['release'].slice(-4)
-        answer += ' (' + year + ')'
+        var year = movie_map[movie]['release'].slice(-4);
+        answer += ' (' + year + ')';
     }
 
-    questions.push(question)
-    answers.push(answer)
+    questions.push(question);
+    answers.push(answer);
 }
 
-var movie = randomItem(Object.keys(tagline_map))
+var movie = randomItem(Object.keys(tagline_map));
+var category = randomItem(movie_map[movie]['categories'].split(","));
+var random_tagline = randomItem(tagline_map[movie]);
 
-var category = randomItem(movie_map[movie]['categories'].split(","))
-var random_tagline = randomItem(tagline_map[movie])
-
-var question = 'What ' + category + ' film has the tagline "' + random_tagline + '"?'
-var answer = movie
+var question = 'What ' + category + ' film has the tagline "' + random_tagline + '"?';
+var answer = movie;
 
 if (!movie.includes('(1') && !movie.includes('(2')) {
-  var year = movie_map[movie]['release'].slice(-4)
-  answer += ' (' + year + ')'
+  var year = movie_map[movie]['release'].slice(-4);
+  answer += ' (' + year + ')';
 }
