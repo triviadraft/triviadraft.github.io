3d8fcd0aecec11f2023c298df71dadc39d3b616d
triviadraft <triviadraft@gmail.com>
2022-04-19 20:21:01 -0700

    alien predator taglines

--- tagline_data.js
+++ tagline_data.js
@@ -1,4 +1,13 @@
 var tagline_map = {
+    "Alien": [
+        "In space no one can hear you scream.",
+        "The Scariest Movie Ever Made. Experience it only on the big screen",
+        "Sometimes the scariest things come from within",
+        "It's Alien, the 8th passenger.",
+        "The scariest movie ever made... just got scarier.",
+        "A word of warning...",
+        "In space no one can hear you scream... This Halloween in theatres, everyone will hear you scream"
+    ],
     "Raiders of the Lost Ark": [
         "Indiana Jones - the new hero from the creators of JAWS and STAR WARS.",
         "The Return of the Great Adventure."
@@ -8,6 +17,23 @@ var tagline_map = {
         "The hero is back!",
         "Trust him.",
         "The man in the hat is back..."
+    ],
+    "Aliens": [
+        "This time there's more.",
+        "This time it's war.",
+        "There are some places in the universe you don't go alone.",
+        "Somewhere in deepest space, all hell is breaking loose.",
+        "They're Even Scarier The Second Time Around!"
+    ],
+    "Predator": [
+        "If it bleeds, we can kill it...",
+        "Nothing like it has ever been on earth before.",
+        "It came for the thrill of the hunt. It picked the wrong man to hunt.",
+        "Soon the hunt will begin.",
+        "Nothing like it has ever been on Earth before. It came from another planet for the thrill of the hunt. It picked the wrong man.",
+        "In a part of the world where there are no rules, deep in the jungle where nothing that lives is safe, an elite rescue squad is being led by the ultimate warrior. But now, they're up against the ultimate enemy. Nothing like it has ever been on earth before. It kills for pleasure, it hunts for sport. But this time, it picked the wrong man to hunt.",
+        "We cannot see it, but it sees the heat of our bodys and the heat of our fear",
+        "In this movie, Arnold will experience every meaning of the word '' Pain ''."
     ],
     "Indiana Jones and the Last Crusade": [
         "He's back in an all new adventure. Memorial Day 1989.",
@@ -23,50 +49,30 @@ var tagline_map = {
         "In the darkest hour, there is a light that shines on every human being...but ONE!",
         "They destroyed everything he had, everything he was. Now, crime has a new enemy and justice has a new face."
     ],
+    "Predator 2": [
+        "Pull over, park, and pray.",
+        "Lions! Tigers! Bears! Oh my!",
+        "He's in town with a few days to kill.",
+        "Silent. Invisible. Invincible. The Ultimate Hunter. This Time... Los Angeles, 1997.",
+        "Silent. Invisible. Invincible. He's in town with a few days to kill.",
+        "Hunting season opens again...",
+        "The Ultimate Hunter.",
+        "This time he's coming to a different kind of jungle",
+        "This Time... Los Angeles, 1997."
+    ],
     "The Rocketeer": [
         "Three years before the United States declares war, Cliff Secord leads America's first battle against the Nazis.",
         "An Ordinary Man Forced to Become An Extraordinary Hero."
     ],
-    "The Shadow": [
-        "The Shadow Knows!",
-        "Who knows what evil lurks in the hearts of men?"
-    ],
-    "The Phantom": [
-        "The ghost who walks. The man who cannot die.",
-        "Slam evil!"
-    ],
-    "The Mask of Zorro": [
-        "When freedom is a memory and justice is outlawed, the just must become outlaws.",
-        "No one has seen his face....but everyone....knows this mark.",
-        "This summer, justice leaves its mark.",
-        "The sword. The mask. The legend. He isn't just one man... HE'S ZORRO!"
-    ],
-    "Pirates of the Caribbean: The Curse of the Black Pearl": [
-        "Prepare to be blown out of the water.",
-        "Over 3000 Islands of Paradise -- For Some it's A Blessing -- For Others... It's A Curse."
-    ],
-    "The Legend of Zorro": [
-        "This Fall, adventure begins with a Z."
-    ],
-    "Pirates of the Caribbean: Dead Man's Chest": [
-        "Captain Jack is back."
-    ],
-    "Pirates of the Caribbean: At World's End": [
-        "At the End of the World, the Adventure Begins"
-    ],
-    "Indiana Jones and the Kingdom of the Crystal Skull": [
-        "In May, the adventure continues.",
-        "The adventure continues . . ."
-    ],
-    "Pirates of the Caribbean: On Stranger Tides": [
-        "Pirate's Life."
-    ],
-    "The Lone Ranger": [
-        "Never Take Off the Mask",
-        "Discover the Untold Story of an American Legend."
-    ],
-    "Pirates of the Caribbean: Dead Men Tell No Tales": [
-        "All pirates must die."
+    "Alien 3": [
+        "In 1979, we discovered in space, no one can hear you scream. In 1992, we will discover, on Earth, everyone can hear you scream.",
+        "In 1979, it came from within. In 1986, it was gone forever. In 1992, our worst fears have come true. It's back.",
+        "It's Back.",
+        "The Bitch is Back",
+        "Start running...again. The bitch is back.",
+        "This time it's hiding in the most terrifying place of all.",
+        "Our worst fears have come true. It's back!",
+        "3 times the suspense. 3 times the danger. 3 times the terror."
     ],
     "Jurassic Park": [
         "Life finds a way.",
@@ -78,9 +84,32 @@ var tagline_map = {
         "An adventure 65 million years in the making.",
         "The most phenomenal discovery of our time... becomes the greatest adventure of all time."
     ],
+    "The Shadow": [
+        "The Shadow Knows!",
+        "Who knows what evil lurks in the hearts of men?"
+    ],
+    "The Phantom": [
+        "The ghost who walks. The man who cannot die.",
+        "Slam evil!"
+    ],
     "The Lost World: Jurassic Park": [
         "Something has survived",
         "They're Walking Our Streets"
+    ],
+    "Alien Resurrection": [
+        "It's been more than 200 years...The beginning has just started.",
+        "We are not alone.",
+        "Witness the resurrection.",
+        "Pray you die first.",
+        "Hell gives birth.",
+        "It's already too late.",
+        "Beyond salvation."
+    ],
+    "The Mask of Zorro": [
+        "When freedom is a memory and justice is outlawed, the just must become outlaws.",
+        "No one has seen his face....but everyone....knows this mark.",
+        "This summer, justice leaves its mark.",
+        "The sword. The mask. The legend. He isn't just one man... HE'S ZORRO!"
     ],
     "Jurassic Park III": [
         "Disaster is an Instinct.",
@@ -103,13 +132,104 @@ var tagline_map = {
         "You know how the dream started. You know how the fantasy ended. Time to evolve from point A to site B. Get ready for the Evolution of Adventure",
         "With every sound, with every heartbeat, there will be more to fear than ever before"
     ],
+    "Pirates of the Caribbean: The Curse of the Black Pearl": [
+        "Prepare to be blown out of the water.",
+        "Over 3000 Islands of Paradise -- For Some it's A Blessing -- For Others... It's A Curse."
+    ],
+    "Alien vs. Predator": [
+        "Whoever wins... We lose.",
+        "It's our planet....It's their war.",
+        "The enemy of my enemy....is my friend."
+    ],
+    "The Legend of Zorro": [
+        "This Fall, adventure begins with a Z."
+    ],
+    "Pirates of the Caribbean: Dead Man's Chest": [
+        "Captain Jack is back."
+    ],
+    "Pirates of the Caribbean: At World's End": [
+        "At the End of the World, the Adventure Begins"
+    ],
+    "Aliens vs. Predator: Requiem": [
+        "This Christmas there will be no peace on Earth.",
+        "The Last Place On Earth We Want To Be Is In The Middle",
+        "The last place we want to be is in the middle.",
+        "It began on their world. It will end on ours.",
+        "In space, no one can hear you scream. On Earth, it won't matter."
+    ],
+    "Indiana Jones and the Kingdom of the Crystal Skull": [
+        "In May, the adventure continues.",
+        "The adventure continues . . ."
+    ],
+    "Predators": [
+        "Fear is Reborn",
+        "The Hunt Is On",
+        "This summer, fear is reborn.",
+        "The most dangerous killers on the planet... but this is not our planet."
+    ],
+    "Pirates of the Caribbean: On Stranger Tides": [
+        "Pirate's Life."
+    ],
+    "Prometheus": [
+        "The search for our beginning could lead to our end",
+        "They went looking for our beginning. What they found could be our end.",
+        "We came from them. They will come for us.",
+        "We Are All Born of Giants",
+        "Do Not Question the Creator",
+        "In the Heavens they found Hell",
+        "Search for Life. Pray for Death",
+        "Welcome to Immortality",
+        "Discover what Mankind was never meant to see",
+        "The Origin of Man is not on Earth",
+        "Fear Your Maker",
+        "The Director of the Original Alien takes you back to the Beginning",
+        "Everything has a Beginning",
+        "Creation Begins 06.08.12",
+        "The Truth is not of this World",
+        "Prepare for the Dawn of Creation",
+        "The Dawn of Creation",
+        "Seek and you shall Find",
+        "They Are Perfect",
+        "Death Has A Beginning",
+        "God is Unknown. They Are Real.",
+        "Get Back to the Ship",
+        "There are Places where Life should not Exist",
+        "They Aren't After You. They Are Inside You.",
+        "They Are Us",
+        "Nothing and No One but Them"
+    ],
+    "The Lone Ranger": [
+        "Never Take Off the Mask",
+        "Discover the Untold Story of an American Legend."
+    ],
     "Jurassic World": [
         "The park is open."
+    ],
+    "Alien: Covenant": [
+        "Run",
+        "The path to paradise begins in hell.",
+        "Hide",
+        "Scream",
+        "Pray",
+        "Discover the Origin of Terror.",
+        "The Beginning of a Nightmare.",
+        "Witness the Creation of Fear.",
+        "Big things have small beginnings."
+    ],
+    "Pirates of the Caribbean: Dead Men Tell No Tales": [
+        "All pirates must die."
     ],
     "Jurassic World: Fallen Kingdom": [
         "Life finds a way.",
         "The park is gone.",
         "Off the Island. Into your Home.",
         "The Next Stage of Evolution."
+    ],
+    "The Predator": [
+        "You'll never see him coming.",
+        "The Hunt has Evolved",
+        "Hunting season opens.",
+        "The Hunt is Bigger.",
+        "Join the Hunt in 2018."
     ]
 }
