3eba2cebe7152ec153e7ac12a0a49e83936978eb
triviadraft <triviadraft@gmail.com>
2022-04-21 12:23:43 -0700

    disable button

--- collect_tagline_data.js
+++ collect_tagline_data.js
@@ -13,7 +13,7 @@ function displayAnswers() {
     document.getElementById('answer3').innerHTML = answers[2];
     document.getElementById('answer4').innerHTML = answers[3];
     document.getElementById('answer5').innerHTML = answers[4];
-    document.getElementById('displayBtn').style.display = 'none';
+    document.getElementById('displayBtn').disabled = true;
 }
 
 function reset() { 
@@ -46,7 +46,7 @@ function reset() {
     document.getElementById('answer3').innerHTML = '';
     document.getElementById('answer4').innerHTML = '';
     document.getElementById('answer5').innerHTML = '';
-    document.getElementById('displayBtn').style.display = '';
+    document.getElementById('displayBtn').disabled = false;
 }
 
 var movies = randomItems(5, Object.keys(tagline_map));
