0d25cf4918781ccba1ad0192f6c5ac6d7a1ade4a
triviadraft <triviadraft@gmail.com>
2022-04-26 02:14:37 -0700

    add comma before and

--- getters/get_movies.js
+++ getters/get_movies.js
@@ -11,7 +11,7 @@ function makeString(arr) {
   if (arr.length === 1) return arr[0];
   const firsts = arr.slice(0, arr.length - 1);
   const last = arr[arr.length - 1];
-  return firsts.join(', ') + ' and ' + last;
+  return firsts.join(', ') + ', and ' + last;
 }
 
 function displayAnswers() {
