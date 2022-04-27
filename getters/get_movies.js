b082d4982f2e773c420a4317e9c635a945e2c388
triviadraft <triviadraft@gmail.com>
2022-04-27 12:46:42 -0700

    disable show all if all hidden

--- getters/get_movies.js
+++ getters/get_movies.js
@@ -17,6 +17,13 @@ function makeString(arr) {
 function showAnswer(n) {
     document.getElementById('answer' + n).innerHTML = answers[n-1];
     document.getElementById('showBtn' + n).hidden = true;
+    if (document.getElementById('showBtn1').hidden &&
+        document.getElementById('showBtn2').hidden &&
+        document.getElementById('showBtn3').hidden &&
+        document.getElementById('showBtn4').hidden &&
+        document.getElementById('showBtn5').hidden) {
+        document.getElementById('showAllBtn').disabled = true;
+    }
 }
 
 function showAnswers() {
