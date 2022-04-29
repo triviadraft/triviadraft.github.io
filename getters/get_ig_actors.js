af4eec9ae87e6e69bb1dcd38bcc564b1e055321f
triviadraft <triviadraft@gmail.com>
2022-04-29 03:59:42 -0700

    change ig actors to 5 question style

--- getters/get_ig_actors.js
+++ getters/get_ig_actors.js
@@ -7,26 +7,91 @@ function randomItems(n, items) {
     return shuffled.slice(0, n);
 }
 
-function displayAnswer() { 
-    document.getElementById('answer').innerHTML = answer;
-    document.getElementById('answer1').innerHTML = answer1;
-    document.getElementById('answer2').innerHTML = answer2;
-    document.getElementById('answer3').innerHTML = answer3;
-    document.getElementById('displayBtn').disabled = true;
+function showAnswer(n) {
+    document.getElementById('answer' + n).innerHTML = answers[n-1];
+    document.getElementById('showBtn' + n).hidden = true;
+    if (document.getElementById('showBtn1').hidden &&
+        document.getElementById('showBtn2').hidden &&
+        document.getElementById('showBtn3').hidden &&
+        document.getElementById('showBtn4').hidden &&
+        document.getElementById('showBtn5').hidden) {
+        document.getElementById('showAllBtn').disabled = true;
+    }
 }
 
-function displayAnswers() { 
-    document.getElementById('answer' + n).innerHTML = answers[n - 1];
-
-    document.getElementById('answer1').innerHTML = answer1;
-    document.getElementById('answer2').innerHTML = answer2;
-    document.getElementById('answer3').innerHTML = answer3;
-    document.getElementById('answer4').innerHTML = answer3;
-    document.getElementById('answer5').innerHTML = answer3;
+function showAnswers() {
+    document.getElementById('answer1').innerHTML = answers[0];
+    document.getElementById('answer2').innerHTML = answers[1];
+    document.getElementById('answer3').innerHTML = answers[2];
+    document.getElementById('answer4').innerHTML = answers[3];
+    document.getElementById('answer5').innerHTML = answers[4];
+    document.getElementById('showBtn1').hidden = true;
+    document.getElementById('showBtn2').hidden = true;
+    document.getElementById('showBtn3').hidden = true;
+    document.getElementById('showBtn4').hidden = true;
+    document.getElementById('showBtn5').hidden = true;
+    document.getElementById('showAllBtn').disabled = true;
 }
 
 function reset() { 
-    var movie = randomItem(Object.keys(ig_actor_map));
+    var movies = randomItems(5, Object.keys(ig_actor_map));
+    questions = [];
+    answers = [];
+    for (const movie of movies) {
+        var category = randomItem(movie_map[movie]['categories'].split(","));
+        var actor_list = ig_actor_map[movie]['supporting'];
+        var random_list = randomItems(3, actor_list);
+
+        var actor1 = random_list[0]['actor'];
+        var actor2 = random_list[1]['actor'];
+        var actor3 = random_list[2]['actor'];
+        var name1 = random_list[0]['name'];
+        if (random_list[0]['description']) {
+            name1 += ' (' + random_list[0]['description'] + ')';
+        }
+        var name2 = random_list[1]['name'];
+        if (random_list[1]['description']) {
+            name2 += ' (' + random_list[1]['description'] + ')';
+        }
+        var name3 = random_list[2]['name'];
+        if (random_list[2]['description']) {
+            name3 += ' (' + random_list[2]['description'] + ')';
+        }
+
+        var question = 'What ' + category + ' film includes ' + actor1 +', ' + actor2 + ', and ' + actor3 + '?';
+        var answer = movie;
+
+        if (!movie.includes('(1') && !movie.includes('(2')) {
+            var year = movie_map[movie]['year'];
+            answer += ' (' + year + ')';
+        }
+
+        questions.push(question);
+        answers.push(answer);
+    }
+
+    document.getElementById("question1").innerHTML = questions[0];
+    document.getElementById("question2").innerHTML = questions[1];
+    document.getElementById("question3").innerHTML = questions[2];
+    document.getElementById("question4").innerHTML = questions[3];
+    document.getElementById("question5").innerHTML = questions[4];
+    document.getElementById('answer1').innerHTML = '';
+    document.getElementById('answer2').innerHTML = '';
+    document.getElementById('answer3').innerHTML = '';
+    document.getElementById('answer4').innerHTML = '';
+    document.getElementById('answer5').innerHTML = '';
+    document.getElementById('showBtn1').hidden = false;
+    document.getElementById('showBtn2').hidden = false;
+    document.getElementById('showBtn3').hidden = false;
+    document.getElementById('showBtn4').hidden = false;
+    document.getElementById('showBtn5').hidden = false;
+    document.getElementById('showAllBtn').disabled = false;
+}
+
+var movies = randomItems(5, Object.keys(ig_actor_map));
+questions = [];
+answers = [];
+for (const movie of movies) {
     var category = randomItem(movie_map[movie]['categories'].split(","));
     var actor_list = ig_actor_map[movie]['supporting'];
     var random_list = randomItems(3, actor_list);
@@ -47,43 +112,14 @@ function reset() {
         name3 += ' (' + random_list[2]['description'] + ')';
     }
 
-    question = 'What ' + category + ' film includes ' + actor1 +', ' + actor2 + ', and ' + actor3 + '?';
-    answer = movie + ':';
-    answer1 = actor1 + ' as ' + name1;
-    answer2 = actor2 + ' as ' + name2;
-    answer3 = actor3 + ' as ' + name3;
-    
-    document.getElementById('question').innerHTML = question;
-    document.getElementById('answer').innerHTML = '';
-    document.getElementById('answer1').innerHTML = '';
-    document.getElementById('answer2').innerHTML = '';
-    document.getElementById('answer3').innerHTML = '';
-    document.getElementById('displayBtn').disabled = false;
-}
+    var question = 'What ' + category + ' film includes ' + actor1 +', ' + actor2 + ', and ' + actor3 + '?';
+    var answer = movie;
 
-var movie = randomItem(Object.keys(ig_actor_map));
-var category = randomItem(movie_map[movie]['categories'].split(","));
-var actor_list = ig_actor_map[movie]['supporting'];
-var random_list = randomItems(3, actor_list);
+    if (!movie.includes('(1') && !movie.includes('(2')) {
+        var year = movie_map[movie]['year'];
+        answer += ' (' + year + ')';
+    }
 
-var actor1 = random_list[0]['actor'];
-var actor2 = random_list[1]['actor'];
-var actor3 = random_list[2]['actor'];
-var name1 = random_list[0]['name'];
-if (random_list[0]['description']) {
-    name1 += ' (' + random_list[0]['description'] + ')';
+    questions.push(question);
+    answers.push(answer);
 }
-var name2 = random_list[1]['name'];
-if (random_list[1]['description']) {
-    name2 += ' (' + random_list[1]['description'] + ')';
-}
-var name3 = random_list[2]['name'];
-if (random_list[2]['description']) {
-    name3 += ' (' + random_list[2]['description'] + ')';
-}
-
-var question = 'What ' + category + ' film includes ' + actor1 +', ' + actor2 + ', and ' + actor3 + '?';
-var answer = movie + ':';
-var answer1 = actor1 + ' as ' + name1;
-var answer2 = actor2 + ' as ' + name2;
-var answer3 = actor3 + ' as ' + name3;
