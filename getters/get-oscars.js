89c728f5b88715c0a24eb89d55635fee56906306
triviadraft <triviadraft@gmail.com>
2022-06-07 23:43:07 -0700

    oscar win data

--- getters/get-oscars.js
+++ getters/get-oscars.js
@@ -4,8 +4,19 @@ function getMapWithNominations(map) {
     return Object.fromEntries(filtered);
 }
 
+function getMapWithWins(map) {
+    const asArray = Object.entries(map);
+    filtered = asArray.filter(([key, value]) => 'leadWins' in map[key] || 'suppWins' in map[key]);
+    return Object.fromEntries(filtered);
+}
+
+
 function reset() { 
-    var actors = randomItems(5, Object.keys(actorMapWithNominations));
+    if (typeof filterVal !== 'undefined' && filterVal === 'Wins') {
+        var actors = randomItems(5, Object.keys(actorMapWithWins));
+    } else {
+        var actors = randomItems(5, Object.keys(actorMapWithNominations));
+    }
 
     questions = [];
     answers = [];
@@ -13,31 +24,65 @@ function reset() {
         var questionTypes = [];
         var leadMovies = actorMapWithNominations[actor]['leadNoms'];
         var suppMovies = actorMapWithNominations[actor]['suppNoms'];
-        if (leadMovies) {
-            questionTypes.push('leadMovie');
-            questionTypes.push('leadYear');
+        var leadWinMovies = actorMapWithNominations[actor]['leadWins'];
+        var suppWinMovies = actorMapWithNominations[actor]['suppWins'];
+        if ((typeof filterVal === 'undefined') || !filterVal || (typeof filterVal !== 'undefined' && filterVal === 'Nominations')) {
+            if (leadMovies) {
+                questionTypes.push('leadMovie');
+                questionTypes.push('leadMovie');
+                questionTypes.push('leadYear');
+            }
+            if (suppMovies) {
+                questionTypes.push('suppMovie');
+                questionTypes.push('suppMovie');
+                questionTypes.push('suppYear');
+            }
         }
-        if (suppMovies) {
-            questionTypes.push('suppMovie');
-            questionTypes.push('suppYear');
+        if ((typeof filterVal === 'undefined') || !filterVal || (typeof filterVal !== 'undefined' && filterVal === 'Wins')) {
+            if (leadWinMovies) {
+                questionTypes.push('leadWinMovie');
+                questionTypes.push('leadWinMovie');
+                questionTypes.push('leadWinYear');
+            }
+            if (suppWinMovies) {
+                questionTypes.push('suppWinMovie');
+                questionTypes.push('suppWinMovie');
+                questionTypes.push('suppWinYear');
+            }
         }
         var questionType = randomItem(questionTypes);
 
         if (questionType == 'leadMovie') {
             var leadMovie = randomItem(leadMovies);
-            var question = actor + ' was nominated for their lead role in what ' + leadMovie['year'] + ' movie?';
+            var question = actor + ' was nominated for an Oscar for their lead role in what ' + leadMovie['year'] + ' movie?';
+            var answer = leadMovie['movie'];
+        } else if (questionType == 'leadWinMovie') {
+            var leadMovie = randomItem(leadWinMovies);
+            var question = actor + ' won an Oscar for their lead role in what ' + leadMovie['year'] + ' movie?';
             var answer = leadMovie['movie'];
         } else if (questionType == 'leadYear') {
             var leadMovie = randomItem(leadMovies);
-            var question = actor + ' was nominated for their lead role in ' + leadMovie['movie'] + ' in what year?';
+            var question = actor + ' was nominated for an Oscar for their lead role in ' + leadMovie['movie'] + ' in what year?';
+            var answer = leadMovie['year'];
+        } else if (questionType == 'leadWinYear') {
+            var leadMovie = randomItem(leadWinMovies);
+            var question = actor + ' won an Oscar for their lead role in ' + leadMovie['movie'] + ' in what year?';
             var answer = leadMovie['year'];
         } else if (questionType == 'suppMovie') {
             var suppMovie = randomItem(suppMovies);
-            var question = actor + ' was nominated for their supporting role in what ' + suppMovie['year'] + ' movie?';
+            var question = actor + ' was nominated for an Oscar for their supporting role in what ' + suppMovie['year'] + ' movie?';
+            var answer = suppMovie['movie'];
+        } else if (questionType == 'suppWinMovie') {
+            var suppMovie = randomItem(suppWinMovies);
+            var question = actor + ' won an Oscar for their supporting role in what ' + suppMovie['year'] + ' movie?';
             var answer = suppMovie['movie'];
         } else if (questionType == 'suppYear') {
             var suppMovie = randomItem(suppMovies);
-            var question = actor + ' was nominated for their supporting role in ' + suppMovie['movie'] + ' in what year?';
+            var question = actor + ' was nominated for an Oscar for their supporting role in ' + suppMovie['movie'] + ' in what year?';
+            var answer = suppMovie['year'];
+        } else if (questionType == 'suppWinYear') {
+            var suppMovie = randomItem(suppWinMovies);
+            var question = actor + ' won for an Oscar for their supporting role in ' + suppMovie['movie'] + ' in what year?';
             var answer = suppMovie['year'];
         }
 
@@ -54,6 +99,7 @@ function reset() {
 }
 
 var actorMapWithNominations = getMapWithNominations(actorMap);
+var actorMapWithWins = getMapWithWins(actorMap);
 var questions = [];
 var answers = [];
 reset();
